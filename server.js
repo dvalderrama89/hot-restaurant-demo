54f979a916c893705c6adb325b82652e4c23f963
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-22 12:01:08 -0700

    connect filepaths

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Data
 const yoda = {
@@ -25,21 +30,68 @@ const obiwankenobi = {
   forcePoints: 1500,
 };
 
+// Customers
+let customers = [
+  {
+    name: 'Test Test',
+    phone: '9512011234',
+    email: 'test@test.com',
+    uniqueID: '1234',
+  }
+];
+
+// Waitlist
+let waitlist = [
+  {
+    name: 'Test Test',
+    phone: '9512011234',
+    email: 'test@test.com',
+    uniqueID: '1234',
+  }
+];
+
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Star Wars Page!');
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-app.get('/yoda', (req, res) => {
-  res.json(yoda);
+app.get('/api/tables', (req, res) => {
+  res.json(customers);
 });
 
-app.get('/darthmaul', (req, res) => {
-  res.json(darthmaul);
+app.get('/api/waitlist', (req, res) => {
+  res.json(waitlist);
 });
 
-app.get('/obiwankenobi', (req, res) => {
-  res.json(obiwankenobi);
+app.get('/tables', (req, res) => {
+  res.sendFile(path.join(__dirname, 'tables.html'));
+});
+
+app.get('/reserve', (req, res) => {
+  res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+// Create New Characters - takes in JSON input
+app.post('/api/tables', (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newCharacter = req.body;
+  console.log(req.body);
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.name = newCharacter.name.replace(/\s+/g, '').toLowerCase();
+  console.log(newCharacter);
+
+  customers.push(newCharacter);
+  res.json(newCharacter);
+});
+
+// Create New Characters - takes in JSON input
+app.post('/api/clear', (req, res) => {
+  customers = [];
+  waitlist = [];
+  res.json(customers);
 });
 
 // Listener
